7a0251ef5934760eb6eb65ed557498ba769e88d6
juanluisgv1 <juanluisgv1@gmail.com>
2025-05-02 18:21:24 +0100

    Remove .next from git tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kIvKMiT9uyVrz3TZinMoOauEowTcKd0e+mT0piHyTpM=\"\n}"
--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_d32cbb62._.js",
-  "static/chunks/src_d8f802b6._.js"
-],
-    source: "dynamic"
-});
