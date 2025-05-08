69403712e4877d86e89c7d5ff054f5ab5495cc7c
Gabriel Melo <gabriel.smelo@proton.me>
2025-05-08 11:51:20 -0300

    feat(pages): implement pages with mock content

--- src/app/(web)/about/page.tsx
+++ src/app/(web)/about/page.tsx
@@ -1,3 +1,36 @@
+import { Container } from '@/components/ui/container'
+import { PageHeader } from '../_components/page-header'
+
 export default function AboutPage() {
-	return <h1>About Page</h1>
+	return (
+		<>
+			<PageHeader
+				title="About"
+				description="We're on a mission to help chefs to aspiring kitchen assistants, we connect passionate food experts with the right opportunities to grow their careers."
+			/>
+
+			<Container className="flex justify-center lg:mt-24">
+				<main className="w-full max-w-[800px]">
+					<h2 className="mb-4 font-bold text-2xl">About Us</h2>
+					<div className="prose">
+						<p>
+							Culinary Jobs is a job discovery platform built specifically for
+							culinary professionals across Canada. From seasoned chefs to
+							aspiring kitchen assistants, we connect passionate food experts
+							with the right opportunities to grow their careers.{' '}
+						</p>
+						<h3>Our Mission </h3>
+						<p>
+							To empower culinary professionals by making job discovery simple,
+							accessible, and tailored to their craft — helping talent find the
+							right kitchens to thrive in. Our Vision To become the leading
+							culinary job platform in Canada, inspiring a stronger, more
+							connected food industry by bridging the gap between culinary
+							talent and opportunity.
+						</p>
+					</div>
+				</main>
+			</Container>
+		</>
+	)
 }
--- src/app/(web)/contact/page.tsx
+++ src/app/(web)/contact/page.tsx
@@ -1,3 +1,22 @@
+import { Container } from '@/components/ui/container'
+import { PageHeader } from '../_components/page-header'
+
 export default function ContactPage() {
-	return <h1>Contact Page</h1>
+	return (
+		<>
+			<PageHeader
+				title="Contact"
+				description="We’re here to help you with anything related to job applications, partnerships, or general inquiries."
+			/>
+
+			<Container className="flex justify-center lg:mt-24">
+				<main className="w-full max-w-[800px]">
+					<h2 className="mb-4 text-center font-bold text-2xl">
+						Have a question, feedback, or need support?
+					</h2>
+					{/* <div className="flex flex-col gap-2"></div> */}
+				</main>
+			</Container>
+		</>
+	)
 }
