ef040139742374bb7d4c6917193aae7b5a7d68fc
Gabriel Melo <gabriel.smelo@proton.me>
2025-05-08 11:16:17 -0300

    fix(jobs-categories): update job category links and improve text styling for consistency

--- src/components/jobs/jobs-categories.tsx
+++ src/components/jobs/jobs-categories.tsx
@@ -5,23 +5,23 @@ import { Badge } from '../ui/badge'
 export function JobsCategories() {
 	return (
 		<div className="mt-2 flex flex-col items-center gap-1 lg:mt-4 lg:flex-row lg:gap-4">
-			<span className="font-normal text-slate-50 text-sm">Jobs categories</span>
+			<b className="font-semibold text-base text-slate-50">Jobs categories</b>
 
 			<div className="flex gap-2">
-				<Link href="/jobs/career/culinary" prefetch>
-					<Badge variant="secondary">Culinary</Badge>
+				<Link href="/jobs/carrear/kitchen-assistant" prefetch>
+					<Badge>Kitchen Assistant</Badge>
 				</Link>
 
-				<Link href="/jobs/career/culinary" prefetch>
-					<Badge variant="secondary">Service</Badge>
+				<Link href="/jobs/carrear/catering" prefetch>
+					<Badge>Catering</Badge>
 				</Link>
 
-				<Link href="/jobs/career/beverage" prefetch>
-					<Badge variant="secondary">Beverage</Badge>
+				<Link href="/jobs/carrear/food-beverage-management" prefetch>
+					<Badge>Food & Beverage Management</Badge>
 				</Link>
 
-				<Link href="/jobs/career/hospitality" prefetch>
-					<Badge variant="secondary">Hospitality</Badge>
+				<Link href="/jobs/carrear/nutrition-dietary" prefetch>
+					<Badge>Nutrition & Dietary</Badge>
 				</Link>
 			</div>
 		</div>
