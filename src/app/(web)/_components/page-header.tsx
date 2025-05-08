93009a08b419c0f161a52ad09faf8b52149fd3a3
Gabriel Melo <gabriel.smelo@proton.me>
2025-05-08 11:37:04 -0300

    feat(page-header): enhance PageHeader component with lead and description props; add mock content for testing

--- src/app/(web)/_components/page-header.tsx
+++ src/app/(web)/_components/page-header.tsx
@@ -3,21 +3,30 @@ import { Container } from '@/components/ui/container'
 
 interface PageHeaderProps {
 	title: string
+	description?: string
+	lead?: string
 }
 
-export function PageHeader({ title }: PageHeaderProps) {
+export function PageHeader({ title, lead, description }: PageHeaderProps) {
 	return (
 		<header className="flex w-full flex-col justify-center bg-accent lg:h-[240px] lg:pt-6 lg:pb-14">
 			<Container className="relative flex flex-col items-center justify-center">
-				<div className="flex h-40 flex-col gap-2 pt-4">
+				<div className="flex h-40 flex-col items-center gap-2 pt-4">
+					{lead && (
+						<div className="mb-2 font-bold text-base text-white uppercase">
+							{lead}
+						</div>
+					)}
+
 					<h1 className="text-center font-extrabold text-2xl text-accent-foreground leading-none lg:text-4xl">
 						{title}
 					</h1>
 
-					<p className="max-w-md text-center text-accent-foreground text-lg">
-						Discover top job opportunities selected for impact, growth, and the
-						next step in your career.
-					</p>
+					{description && (
+						<p className="max-w-md text-center text-accent-foreground text-lg">
+							{description}
+						</p>
+					)}
 				</div>
 
 				<div className="-bottom-24 absolute mx-auto">
