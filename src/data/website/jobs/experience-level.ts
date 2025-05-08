8dd677b43f6bc263508db60fd5c07fb977116157
Gabriel Melo <gabriel.smelo@proton.me>
2025-05-08 00:41:01 -0300

    feat: add experience levels and featured jobs content; remove outdated featured jobs file

---
+++ src/data/website/jobs/experience-level.ts
@@ -0,0 +1,14 @@
+export const experienceLevel = [
+	{
+		value: 'entry-level',
+		label: 'Entry level',
+	},
+	{
+		value: 'intermediate',
+		label: 'Intermediate',
+	},
+	{
+		value: 'expect',
+		label: 'Expect',
+	},
+] as const
--- src/data/website/sections/feactured-jobs.ts
+++
@@ -1,9 +0,0 @@
-export const contentFeacturedJobs = {
-	title: "Featured Jobs Openings",
-	subtitle:
-		"Discover top job opportunities selected for impact, growth, and the next step in your career.",
-	callToAction: {
-		target: "/",
-		text: "See All Jobs",
-	},
-};
