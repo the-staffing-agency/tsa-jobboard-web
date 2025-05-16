160a7cca8bec0bb42486c0bdd2aff673f04aff0f
Gabriel Melo <gabriel.smelo@proton.me>
2025-05-16 11:57:06 -0300

    feat(theme): add ThemeProvider component and integrate theming support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
+import { ThemeProvider } from '@/contexts/theme-provider'
 import { metadataConfig } from '@/data/website/metadata'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,8 +22,10 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<ThemeProvider defaultTheme="default">{children}</ThemeProvider>
+			</body>
 		</html>
 	)
 }
