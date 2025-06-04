6cca6124fe383f9eb8fbcad351c9f34257491c88
Gabriel Melo <gabriel_smelo@proton.me>
2025-06-03 22:04:45 -0300

    feat: implement api service for handling API requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import { env } from '@/config/env'
 
 export function api(path: string, init?: RequestInit) {
 	const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
-	const apiPrefix = '/api'
+
+	const apiPrefix = '/api/v1'
 
 	const url = new URL(apiPrefix.concat(path), baseUrl)
 
