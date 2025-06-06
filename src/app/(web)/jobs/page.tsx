5da6593f2d1f8496ef85110a59989c165939c563
Gabriel Melo <gabriel_smelo@proton.me>
2025-06-06 09:01:59 -0300

    feat: implement LastestJobs component to display latest job listings

--- src/app/(web)/jobs/page.tsx
+++ src/app/(web)/jobs/page.tsx
@@ -1,7 +1,7 @@
+import { LastestJobs } from '@/components/blocks/latest-jobs'
 import { FilterAsideJobForm } from '@/components/forms/filter-aside-job-form'
 import { SearchJobFrom } from '@/components/forms/search-job-form'
 import { Container } from '@/components/ui/container'
-import { Jobs } from '@/components/ui/job/jobs'
 import { ListView } from '@/components/ui/list-view'
 import { Widget } from '@/components/widget'
 import { pageHeaderContentMock } from '@/data/website/pages/page-header'
@@ -17,19 +17,11 @@ export default async function SearchPage() {
 				</Suspense>
 			</PageHeader>
 			<Container className="mt-10 flex flex-col justify-center gap-10 lg:flex-row">
-				<aside className="relative flex-1/4 lg:min-w-sm">
-					<div className="sticky top-10">
-						<Widget>
-							<span className="mb-4 block font-bold text-2xl">Filter</span>
-							<FilterAsideJobForm />
-						</Widget>
-					</div>
-				</aside>
-				<main className="w-full">
+				<main className="w-full max-w-[800px]">
 					<h2 className="mb-4 font-bold text-2xl">Recent Jobs</h2>
 					<ListView>
 						<Suspense fallback={<div>Loading...</div>}>
-							<Jobs />
+							<LastestJobs />
 						</Suspense>
 					</ListView>
 				</main>
