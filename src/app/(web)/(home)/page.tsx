3fd26195803987dc210a3f7c7aa413d46d409eec
Gabriel Melo <gabriel_smelo@proton.me>
2025-06-13 08:59:49 -0300

    feat: restructure HomePage layout and add Container for Recent Jobs section

--- src/app/(web)/(home)/page.tsx
+++ src/app/(web)/(home)/page.tsx
@@ -5,8 +5,11 @@ import {
 	HeroSubtitle,
 	HeroTitle,
 } from '@/components/blocks/hero'
+import { LastestJobs } from '@/components/blocks/latest-jobs'
 import { SearchJobFrom } from '@/components/forms/search-job-form'
+import { Container } from '@/components/ui/container'
 import { JobCategories } from '@/components/ui/job/job-categories'
+import { ListView } from '@/components/ui/list-view'
 import { mockHeroData } from '@/data/website/hero'
 import { Suspense } from 'react'
 
@@ -23,9 +26,15 @@ export default async function HomePage() {
 					<Suspense>
 						<SearchJobFrom />
 					</Suspense>
-					<JobCategories />
 				</HeroFooter>
 			</Hero>
+
+			<Container className="mt-10 flex flex-col justify-center gap-10 lg:flex-row">
+				<main className="w-full max-w-[800px]">
+					<h2 className="mb-4 font-bold text-2xl">Recent Jobs</h2>
+					<LastestJobs />
+				</main>
+			</Container>
 		</div>
 	)
 }
