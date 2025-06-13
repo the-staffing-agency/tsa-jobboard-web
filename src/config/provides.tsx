de161094febb21566e10c48dfc8b07e8aa31919e
Gabriel Melo <gabriel_smelo@proton.me>
2025-06-13 10:46:56 -0300

    feat: migrate api and queryClient imports to lib directory, add queryClient and api files

--- src/config/provides.tsx
+++ src/config/provides.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { queryClient } from '@/services/query-client'
+import { queryClient } from '@/lib/query-client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
