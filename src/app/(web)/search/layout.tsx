300c6cad3cbaa83f16d0465d0a969e98ed9c6407
Gabriel Melo <gabriel_smelo@proton.me>
2025-06-15 00:05:50 -0300

    feat: implement search page template with job results and filters

--- src/app/(web)/search/layout.tsx
+++ src/app/(web)/search/layout.tsx
@@ -1,43 +1,14 @@
-import { PortalFilters } from '@/components/blocks/portal-filters'
-import { SearchJobFrom } from '@/components/forms/search-job-form'
-import {
-	PageAsideDefault,
-	PageHeader,
-	PageMainDefault,
-} from '@/components/templates'
-import { Container } from '@/components/ui/container'
-import { Widget, WidgetContent, WidgetTitle } from '@/components/widget'
-import { pageHeaderContentMock } from '@/data/website/pages/page-header'
-import { Suspense } from 'react'
+import type { ComponentProps } from 'react'
 
-interface JobTemplateProps {
-	title: string
-	children: React.ReactNode
-}
+interface SearchPageTemplateProps extends ComponentProps<'section'> {}
 
-export default function SearchLayout({ title, children }: JobTemplateProps) {
+export default function SearchPageTemplate({
+	children,
+}: SearchPageTemplateProps) {
 	return (
-		<>
-			<PageHeader title={title} description={pageHeaderContentMock.description}>
-				<Suspense>
-					<SearchJobFrom />
-				</Suspense>
-			</PageHeader>
-
-			<Container className="mt-10 flex flex-col justify-center gap-10 lg:flex-row">
-				<PageAsideDefault>
-					<Widget>
-						<WidgetTitle>Filter</WidgetTitle>
-						<WidgetContent>
-							<PortalFilters />
-						</WidgetContent>
-					</Widget>
-				</PageAsideDefault>
-
-				<PageMainDefault>{children}</PageMainDefault>
-			</Container>
-
+		<section>
+			{children}
 			<footer className="mt-20" />
-		</>
+		</section>
 	)
 }
