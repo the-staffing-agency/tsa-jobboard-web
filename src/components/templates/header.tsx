7033aa3b3caa4a76cd659fe175fc57485672dc4d
Gabriel Melo <gabriel_smelo@proton.me>
2025-06-17 12:13:20 -0300

    refactor: adjust header container alignment and update accent color variable

--- src/components/templates/header.tsx
+++ src/components/templates/header.tsx
@@ -7,7 +7,7 @@ export function Header({ children, ...props }: ComponentProps<'header'>) {
 			className="relative z-50 border-white/10 border-b bg-accent"
 			{...props}
 		>
-			<Container className="flex h-16 items-center justify-center lg:h-20">
+			<Container className="flex h-16 items-center lg:h-20">
 				{children}
 			</Container>
 		</header>
