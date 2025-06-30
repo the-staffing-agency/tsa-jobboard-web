932cbc725a27c4f43c212f5ef7dcd4ee55aa7679
Gabriel Melo <gbsmlo@gmail.com>
2025-06-30 17:26:02 -0300

    refactor: update layout and styling in JobSinglePage and PageAsideDefault components

--- src/components/templates/page-aside-default.tsx
+++ src/components/templates/page-aside-default.tsx
@@ -4,7 +4,7 @@ export interface PageAsideProps extends ComponentProps<'aside'> {}
 
 export function PageAsideDefault({ children, ...props }: PageAsideProps) {
 	return (
-		<aside className="relative flex-1/4 lg:min-w-sm" {...props}>
+		<aside className="relative flex-1/4 lg:max-w-[380px]" {...props}>
 			<div className="lg:sticky lg:top-10">{children}</div>
 		</aside>
 	)
--- src/templates/single/job-single-page.tsx
+++ src/templates/single/job-single-page.tsx
@@ -14,32 +14,36 @@ interface JobSinglePage {
 
 export function JobSinglePage({ content }: JobSinglePage) {
 	return (
-		<Container className="flex lg:mt-10 lg:gap-10">
-			<article className="max-w-[800px] flex-3/4">
-				<header className="flex flex-col gap-1 lg:mb-6">
-					<h1 className="font-bold text-xl lg:text-3xl">{content.title}</h1>
-				</header>
+		<Container className="py-6 lg:py-10">
+			<div className="justify-baseline flex flex-col gap-4 pb-10 lg:flex-row lg:gap-10 lg:pb-0">
+				<article className="max-w-[800px] flex-3/4">
+					<header className="flex flex-col gap-1 lg:mb-6">
+						<h1 className="font-extrabold text-3xl lg:text-4xl">
+							{content.title}
+						</h1>
+					</header>
 
-				<main className="prose">
-					{content.description && (
-						<div className="flex flex-col gap-2">
-							<h2 className="text-2xl">Job Details</h2>
-							<SanitizedHtml html={content.description} />
-						</div>
-					)}
-				</main>
-			</article>
+					<main className="prose">
+						{content.description && (
+							<div className="flex flex-col gap-2">
+								<h2 className="text-2xl">Job Details</h2>
+								<SanitizedHtml html={content.description} />
+							</div>
+						)}
+					</main>
+				</article>
 
-			<PageAsideDefault>
-				<Widget>
-					<WidgetTitle>Apply Now</WidgetTitle>
-					<WidgetContent>
-						<div className="w-full">
-							<JobApplicationForm id={content.id} />
-						</div>
-					</WidgetContent>
-				</Widget>
-			</PageAsideDefault>
+				<PageAsideDefault>
+					<Widget className="fixed bottom-0 left-0 w-full lg:relative lg:text-center">
+						<WidgetTitle className="hidden lg:block">Apply Now</WidgetTitle>
+						<WidgetContent>
+							<div className="w-full">
+								<JobApplicationForm id={content.id} />
+							</div>
+						</WidgetContent>
+					</Widget>
+				</PageAsideDefault>
+			</div>
 		</Container>
 	)
 }
