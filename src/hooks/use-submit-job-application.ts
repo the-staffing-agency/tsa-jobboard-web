6007e80907a25558ae30d299f63645ddfaeb0cb6
Gabriel Melo <gbsmlo@gmail.com>
2025-06-30 16:38:53 -0300

    feat: enhance job application submission logic with applicant data handling

--- src/hooks/use-submit-job-application.ts
+++ src/hooks/use-submit-job-application.ts
@@ -1,14 +1,27 @@
+'use client'
+
 import { submitJobApplication } from '@/http/submit-job-application'
 import { queryClient } from '@/lib/query-client'
 import { useMutation } from '@tanstack/react-query'
+import { useAppliedJobsLocal } from './use-job-applied-local'
 
 const JOB_APPLICATION_QUERY_KEY = ['job-application']
 
 export function useSubmitJobApplication() {
-	const { mutateAsync, isPending, isSuccess, isError } = useMutation({
+	const { addToApplied } = useAppliedJobsLocal()
+
+	const { mutateAsync, isSuccess, isPending, isError } = useMutation({
 		mutationFn: submitJobApplication,
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: JOB_APPLICATION_QUERY_KEY })
+		onSuccess: (_response, variables) => {
+			addToApplied({
+				applicant: {
+					name: variables.data.first_name,
+					lastname: variables.data.last_name,
+					email: variables.data.email,
+				},
+				id: variables.id.toString(),
+			})
+			queryClient.cancelQueries({ queryKey: JOB_APPLICATION_QUERY_KEY })
 		},
 		onError: () => {
 			queryClient.invalidateQueries({ queryKey: JOB_APPLICATION_QUERY_KEY })
