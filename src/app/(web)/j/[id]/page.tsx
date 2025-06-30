7ff9104156ff6a49eaa2949cfb0d54b00d604ef7
Gabriel Melo <gbsmlo@gmail.com>
2025-06-30 16:51:54 -0300

    feat: add generateMetadata function to fetch job title for SEO

--- src/app/(web)/j/[id]/page.tsx
+++ src/app/(web)/j/[id]/page.tsx
@@ -1,41 +1,17 @@
 import { getJobById } from '@/http/get-job-by-id'
 import { JobSinglePage } from '@/templates/single/job-single-page'
+import type { Metadata } from 'next'
 
-// function getPageTitle(title: string) {
-// 	const titleCapitalize = capitalizeWords(title.replaceAll('-', ' '))
-// 	return titleCapitalize ? titleCapitalize : 'Jobs'
-// }
-
-// export async function generateMetadata({
-// 	id,
-// }: {
-// 	params: Promise<{
-// 		id: string
-// 	}>
-// }) {
-// 	// const [firstParamsCanBeIdOrSlug] = (await params).params
-
-// 	// const isParamsId = isNumeric(firstParamsCanBeIdOrSlug)
-
-// 	// if (isParamsId) {
-// 	// 	const [id] = (await params).params
-// 	// 	jobContent = await getJobById(id)
-
-// 	// 	return {
-// 	// 		title: jobContent.title,
-// 	// 	}
-// 	// }
-
-// 	// if (!isParamsId) {
-// 	// 	const [slug] = (await params).params
-
-// 	// 	pageTitle = getPageTitle(slug.replace(/^chef-/, ''))
-
-// 	// 	return {
-// 	// 		title: slug.replaceAll('-', ' ').toUpperCase(),
-// 	// 	}
-// 	// }
-// }
+export async function generateMetadata({
+	params,
+}: {
+	params: { id: string }
+}): Promise<Metadata> {
+	const { data } = await getJobById({ id: params.id })
+	return {
+		title: data.title,
+	}
+}
 
 export default async function Page({
 	params,
