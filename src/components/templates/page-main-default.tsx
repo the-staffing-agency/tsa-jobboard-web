4c616a0edd15ba3e86913e753c98c4e1d1d3e70b
Gabriel Melo <gbsmlo@gmail.com>
2025-07-16 13:33:07 -0300

    feat: replace SearchPageTemplate with NoSidebarPageTemplate and add new template component

--- src/components/templates/page-main-default.tsx
+++ src/components/templates/page-main-default.tsx
@@ -4,7 +4,7 @@ interface PageMainProps extends ComponentProps<'main'> {}
 
 export function PageMainDefault({ children }: PageMainProps) {
 	return (
-		<main className="flex-3/4">
+		<main className="w-full max-w-[800px]">
 			<div className="flex flex-col gap-4 lg:gap-6">{children}</div>
 		</main>
 	)
